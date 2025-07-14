7f3960dd8f29274cc532d86ffddcebe3db50d6f9
Titayna <Titayna.kauffmann@gmail.com>
2025-07-14 20:09:27 +0000

    Deploying to gh-pages from @ Titayna/titayna.github.io@e9335810e4f2ed4c0ada40297790fea0055f8810 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,28 +466,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%69%74%61%79%6E%61.%6B%61%75%66%66%6D%61%6E%6E@%75%6E%69.%6C%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/titayna", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://orcid.org/0009-0000-7139-4439", "_blank");
         },
       },{
         id: 'social-custom_social',
