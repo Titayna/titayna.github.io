f226b432fa7fdd9867f5e55744899c620b471dcf
Titayna <Titayna.kauffmann@gmail.com>
2025-07-27 17:45:50 +0000

    Deploying to gh-pages from @ Titayna/titayna.github.io@d39b89d54bdad8abafa987fb98c7f9e68dee2961 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "PROJECTS",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
