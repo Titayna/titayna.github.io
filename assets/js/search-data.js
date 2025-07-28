66d4c6c12c1602ba6cc2d59a8e9168a6a7efee40
Titayna <Titayna.kauffmann@gmail.com>
2025-07-28 16:11:54 +0000

    Deploying to gh-pages from @ Titayna/titayna.github.io@b47e46514bdae23be4c5998969bbaae68b502d99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,7 +452,7 @@ ninja.data = [{
           title: 'Notion Template for PhD Planning',
           description: "A customizable Notion planner designed to help PhD students track their research, deadlines, publications, and goals in one structured space.",
           section: "Ressources",handler: () => {
-              window.location.href = "/ressources/notion-phd-template/";
+              window.location.href = "/ressources/2_ressources/";
             },},{
         id: 'social-email',
         title: 'email',
