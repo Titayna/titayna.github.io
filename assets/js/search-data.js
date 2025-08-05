5b1e8536772b9496c9db595fb7f3a13da06d47bd
Titayna <Titayna.kauffmann@gmail.com>
2025-08-05 12:47:06 +0000

    Deploying to gh-pages from @ Titayna/titayna.github.io@78c4be803e95a25e02a3ad79b9bc50300f271a6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,18 +71,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-apple-lisa-1983",
+            },},{id: "projects-apple-lisa-1983",
           title: 'Apple Lisa (1983)',
           description: "Case study",
           section: "Projects",handler: () => {
