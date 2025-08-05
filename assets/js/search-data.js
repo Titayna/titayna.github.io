ea73ce3459096fb84fa521143a8c4dc326d27c9a
Titayna <Titayna.kauffmann@gmail.com>
2025-08-05 15:43:51 +0000

    Deploying to gh-pages from @ Titayna/titayna.github.io@4f073e2f4430a47545847d7f5c8fc0be132e4075 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -91,6 +91,11 @@ ninja.data = [{
           description: "Heritagisation of code",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-cahiers-polier",
+          title: 'Cahiers Polier',
+          description: "Handwritten Text Recognition",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{id: "ressources-prompt-engineering-guidebook",
           title: 'Prompt Engineering Guidebook',
           description: "A compact guide to writing prompts for generative AI models.",
